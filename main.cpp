cddce510c726f861a60f6f59e2e327b0c71b430b
kaixiang1212 <32703443+kaixiang1212@users.noreply.github.com>
2019-06-22 18:21:44 +1000

    changes made to be testable

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-// Copyright [2019] Kai Xiang Yong z5175681
-// COMP6771 Assignment 1 Word Ladder
 #include <iostream>
 
 #include "assignments/wl/lexicon.h"
@@ -9,28 +7,7 @@ int main() {
   auto lexicon = GetLexicon("assignments/wl/words.txt");
 
   // TODO(students): Replace this with your code
-  std::string start, end;
-  std::cout << "Enter start word (RETURN to quit): ";
-  getline(std::cin, start);
-  if (start == "")
-    return 0;
-  std::cout << "Enter destination word: ";
-  getline(std::cin, end);
-  if (start.length() != end.length()) {
-    std::cout << "No ladder found."
-              << "\n";
-    return 1;
+  for (const auto& word : lexicon) {
+    std::cout << word << '\n';
   }
-  for (std::size_t i = 0; i < start.length(); i++) {
-    if (start.at(i) < 'a' || start.at(i) > 'z' || end.at(i) < 'a' ||
-        end.at(i) > 'z') {
-      std::cout << "No ladder found."
-                << "\n";
-      return 1;
-    }
-  }
-  preprocess(lexicon, start);
-
-  findPaths(start, end);
-  return 0;
 }
